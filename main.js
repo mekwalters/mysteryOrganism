4625d239cb059e8c014e2faf5fd32d9351406a62
mekwalters <mekwalters@gmail.com>
2023-07-06 20:50:53 -0400

    Chat GPT Fixes .mutate method

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 // Returns a random DNA base
 const returnRandBase = () => {
-  const dnaBases = ['A', 'T', 'C', 'G'];
+  const dnaBases = ["A", "T", "C", "G"];
   return dnaBases[Math.floor(Math.random() * 4)];
 };
 
-// Returns a random single stand of DNA containing 15 bases
+// Returns a random single strand of DNA containing 15 bases
 const mockUpStrand = () => {
   const newStrand = [];
   for (let i = 0; i < 15; i++) {
@@ -12,6 +12,29 @@ const mockUpStrand = () => {
   }
   return newStrand;
 };
+
+// pAequor factory
+const pAequorFactory = (num, arr) => {
+  return {
+    specimenNum: num,
+    dna: arr,
+    mutate: function () {
+      const mutatedDna = this.dna.map((base) => {
+        let newBase = returnRandBase();
+        while (newBase === base) {
+          newBase = returnRandBase();
+        }
+        return newBase;
+      });
+
+      return mutatedDna;
+    },
+  };
+};
+
+const organism = pAequorFactory(1, mockUpStrand());
+console.log("Original DNA:", organism.dna);
+console.log("Mutated DNA:", organism.mutate());
 
 
 
